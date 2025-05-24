e7d61503618c17782a3e16a539bea18f643917eb
PietroLovison <pietrolovison25@gmail.com>
2025-05-24 11:05:16 -0300

    correcao prova

--- correcaoProva.java
+++ correcaoProva.java
@@ -5,7 +5,7 @@ public class correcaoProva{
 
         String opcao = "0"; //inicializo minha variavel com um valor inexistente nas opções.
         while (opcao != "9"){
-            opcao = menu() //signfica retorno da função menu
+            opcao = menu(); //signfica retorno da função menu
         }
 
 
@@ -13,15 +13,15 @@ public class correcaoProva{
 
     public static String menu(){
         //Display do menu
-        System.out.println("Menu")
+        System.out.println("Menu");
         //etc
 
         // agora pede pro usuario digitar a opcap desejada
-        Scanner escolha = new Scanner(System.in);
+        Scanner escolhe = new Scanner(System.in);
         String item = escolhe.next();
 
         //retorna o valor selecionado para a função main
-        return 
+        return item;
 
     }
 
